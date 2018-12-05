625f39674b0b1dc7551e2d67495754725436ede3
Jaron Rademeyer <jaron.rademeyer@poort.co>
2018-12-05 17:24:04 +0200

    * Added programme_names process to clean names, identified via ProgNo

---
+++ programme_names.py
@@ -0,0 +1,88 @@
+import csv
+
+from datapackage_pipelines.wrapper import process
+from pprint import pformat
+from slugify import slugify
+import logging
+import os
+import requests
+import yaml
+
+portal_url = os.environ.get('PORTAL_URL', "https://dynamicbudgetportal.openup.org.za/")
+
+programme_names = {
+    'national': {},
+    'provincial': {},
+}
+warned = {}
+
+
+def modify_datapackage(datapackage, parameters, stats):
+    # No datapackage modification; acts as an initialization
+    year_slug = parameters['financial_year']
+    sphere = parameters['sphere']
+    listing_url_path = year_slug + '/' + sphere + '/programmes.csv'
+    listing_url = portal_url + listing_url_path
+    logging.info(listing_url)
+    r = requests.get(listing_url)
+    r.raise_for_status()
+    decoded_content = r.content.decode('utf-8')
+    csv_reader = csv.reader(decoded_content.splitlines())
+
+    first = False
+    for row in csv_reader:
+        if not first:  # skip headers
+            first = True
+            continue
+        government = row[0]
+        department = row[1]
+        programme_name = row[2]
+        programme_number = row[3]
+        try:
+            programme_names[sphere][government]
+        except KeyError:
+            programme_names[sphere][government] = {}
+        try:
+            programme_names[sphere][government][department]
+        except KeyError:
+            programme_names[sphere][government][department] = {}
+        programme_names[sphere][government][department][programme_number] = programme_name
+
+    logging.info(programme_names)
+    return datapackage
+
+
+def process_row(row, row_index,
+                resource_descriptor, resource_index,
+                parameters, stats):
+    prog_name_key = parameters.get('programme_name_column', 'Programme')
+    prog_no_key = parameters.get('programme_number_column', 'ProgNo')
+    dept_name_key = parameters.get('department_column', 'Department')
+    gov_key = parameters.get('government_column', 'Province')
+    sphere = parameters['sphere']
+
+    if sphere == 'national':
+        government_name = 'South Africa'
+    elif sphere == 'provincial':
+        government_name = row[gov_key]
+    else:
+        raise Exception("Invalid sphere parameter")
+
+    department_name = row[dept_name_key]
+    auth_prog_name \
+        = programme_names[sphere][government_name][department_name].get(str(row[prog_no_key]), None)
+    if auth_prog_name:
+        row[prog_name_key] = auth_prog_name
+    else:
+        warning_key = (government_name, row[dept_name_key], row[prog_name_key])
+        if warning_key not in warned:
+            logging.warning("No authoritative programme name found for %s - %s - %s (#%s)",
+                            government_name, row[dept_name_key], row[prog_name_key], row[prog_no_key])
+            logging.info("Tried: programme_names[%s][%s][%s].get(row[%s] = %s)" %
+                         (sphere, government_name, department_name, prog_no_key, row[prog_no_key]))
+            warned[warning_key] = True
+    return row
+
+
+process(modify_datapackage=modify_datapackage,
+        process_row=process_row)
